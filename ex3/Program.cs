ae707776ec3a8471d314d90009d3fc456361982b
Admin <Admin@localhost>
2024-05-31 18:45:07 +0300

    Finish ex.3

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -5,3 +5,50 @@
 //
 // Пример:
 // [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+void InputVector(int[] vector, int start)
+{
+    if (start == vector.GetLength(0))
+    {
+        return;
+    }
+    Console.WriteLine("Введите " + (start + 1) + " элемент массива");
+    vector[start] = Convert.ToInt32(Console.ReadLine());
+    InputVector(vector, start + 1);
+}
+
+void PrintVector(int[] vector, int start)
+{
+    if (start == vector.GetLength(0) - 1)
+    {
+        Console.Write(vector[start]);
+        return;
+    }
+    Console.Write(vector[start] + ", ");
+    PrintVector(vector, start + 1);
+}
+
+void InvertPrintVector(int[] vector, int end)
+{
+    if (end == 0)
+    {
+        Console.Write(vector[end]);
+        return;
+    }
+    Console.Write(vector[end] + ", ");
+    InvertPrintVector(vector, end - 1);
+}
+
+int n = 0;
+
+Console.WriteLine("Введите длину массива: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+
+InputVector(array, 0);
+
+Console.Write("[");
+PrintVector(array, 0);
+Console.Write("] => [");
+InvertPrintVector(array, array.GetLength(0) - 1);
+Console.WriteLine("]");
